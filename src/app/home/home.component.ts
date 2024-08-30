2422a650ab81b746ca766d92faa80fd0b5c2419f
santoslinares1 <slinares@mi.unc.edu.ar>
2024-08-29 23:15:54 -0300

    cambios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -8,5 +9,4 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
 }
