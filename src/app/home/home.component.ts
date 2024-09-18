039b2b89afe6ca2d53997a8eaa8295f50c16899d
MCazas <mati.cazas99@gmail.com>
2024-09-18 14:18:45 -0300

    sliders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  images: string[] = ['uno.jpeg','dos.jpeg','tres.jpeg'];
+  currentIndex = 0;
+  translateValue = 0; 
+
+
+  prevSlide(): void {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.images.length - 1;
+    } else {
+      this.currentIndex--;
+    }
+    this.updateSlider();
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex === this.images.length - 1) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+    this.updateSlider();
+  }
+
+  updateSlider(): void {
+    this.translateValue = -100 * this.currentIndex;
+    console.log(this.currentIndex)
+  }
 }
