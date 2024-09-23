b95cd8d44cb98ed5906db85362c74c3cd0e3b28a
santoslinares1 <slinares@mi.unc.edu.ar>
2024-09-23 14:59:05 -0300

    mejoras y deploy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -9,33 +9,54 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
   images: string[] = ['uno.jpeg','dos.jpeg','tres.jpeg'];
   currentIndex = 0;
   translateValue = 0; 
+  private intervalId: any;
 
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.startAutoSlide();
+    });
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.stopAutoSlide();
+    this.intervalId = setInterval(() => {
+      this.ngZone.run(() => {
+        this.nextSlide();
+      });
+    }, 3000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
 
   prevSlide(): void {
-    if (this.currentIndex === 0) {
-      this.currentIndex = this.images.length - 1;
-    } else {
-      this.currentIndex--;
-    }
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.images.length - 1;
     this.updateSlider();
   }
 
   nextSlide(): void {
-    if (this.currentIndex === this.images.length - 1) {
-      this.currentIndex = 0;
-    } else {
-      this.currentIndex++;
-    }
+    this.currentIndex = (this.currentIndex < this.images.length - 1) ? this.currentIndex + 1 : 0;
     this.updateSlider();
   }
 
   updateSlider(): void {
     this.translateValue = -100 * this.currentIndex;
-    console.log(this.currentIndex)
   }
+
 }
